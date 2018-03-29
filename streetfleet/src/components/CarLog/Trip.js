598e34fce8c6effadd8b92194fb0b47f5ed952b0
Marce Ramirez <marceram87@gmail.com>
2018-03-29 11:55:09 +0200

    fix: error in trip.js

--- streetfleet/src/components/CarLog/Trip.js
+++ streetfleet/src/components/CarLog/Trip.js
@@ -1,7 +1,8 @@
-import React from 'react';
-// import { Table } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
 import moment from 'moment';
 
+export const Trip = (props) => {
   return (
     <tr>
       <td>{moment(props.trip.start_time).format('lll')}</td>
