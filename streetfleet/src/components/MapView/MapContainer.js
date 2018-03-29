c06350b671b8f677bd8f639b7a7fb104be9a9768
Niklas Wibelius <niklas@wibelius.se>
2018-03-29 17:41:39 +0200

    feat: livemap fetch initial commit

--- streetfleet/src/components/MapView/MapContainer.js
+++ streetfleet/src/components/MapView/MapContainer.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps"
+import React, { Component } from 'react';
+
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, Polyline } from "react-google-maps"
+
+import { connect } from 'react-redux';
+
+import Actions from '../../store/actions/cars.actions';
+import svg from '../../img/map-car-marker.svg'
+
+
+
+
+const renderCarMarkers = (props) => {
+  return props.cars.map(car => {
+    return (
+      <Marker
+        key={car._id}
+        className="svg"
+        position={{ lat: props.lastLocations.latitude, lng: props.lastLocations.longitude }}
+      />)
+  })
+}
 
 const MapContainer = withScriptjs(withGoogleMap(props =>
-  <GoogleMap
-    defaultZoom={16}
-    defaultCenter={{ lat: 41.404082, lng: 2.175017 }}
-  ></GoogleMap>
+  <GoogleMap defaultZoom={14} center={{ lat: props.lastLocations.latitude, lng: props.lastLocations.longitude }}   >
+    {renderCarMarkers(props)}
+  </GoogleMap>
 ))
 
-export default MapContainer;
+const mapStateToProps = (state) => ({
+  trips: state.cars.trips,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getTrips: (mac_address) => { dispatch(Actions.getTrips(mac_address)) },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MapContainer);
+
--- streetfleet/src/components/MapView/MapView.js
+++ streetfleet/src/components/MapView/MapView.js
@@ -1,24 +1,71 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import Actions from '../../store/actions/cars.actions';
 import config from '../../config';
 import MapContainer from './MapContainer';
 import VehicleModal from './../VehicleModal';
 import '../../css/Map.css';
 
+// This function loops through the cars in the store and looks up there last position
+
 class MapView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 30.345345,
+      longtitude: 2.175017
+    }
+    setInterval(() => this.processLocations(), 2000);
+
+  }
+
+  componentWillMount = async () => {
+    await this.props.getCars();
+  }
+
+  lastLocations = {};
+
+  processLocations = () => {
+    console.log('CARS: ', this.props.cars);
+
+    this.props.cars.map(car => {
+      this.props.getTrips(car.mac_address)
+      this.lastLocations = this.props.trips.pop().locations.pop();
+      console.log('LAST LOCATIONS:', this.lastLocations);
+      console.log('CAR:', car);
+    })
+  }
+
+
   render() {
+    console.log(this.state);
     return (
       <div className="row MapView">
         <MapContainer
+          latitude={this.state.latitude}
+          longtitude={this.state.longtitude}
           googleMapURL={config.googleMapURL}
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `calc(100vh - 62px)` }} />}
           mapElement={<div style={{ height: `100%` }} />}
+          cars={this.props.cars}
+          lastLocations={this.lastLocations}
         />
-        <VehicleModal/>
+        <VehicleModal cars={this.props.cars} />
       </div>
     )
   }
 }
+const mapStateToProps = (state) => ({
+  cars: state.cars.cars,
+  trips: state.cars.trips,
+});
 
-export default MapView;
+const mapDispatchToProps = (dispatch) => ({
+  getCars: () => { dispatch(Actions.getCars) },
+  getTrips: (mac_address) => { dispatch(Actions.getTrips(mac_address)) },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MapView);
+
