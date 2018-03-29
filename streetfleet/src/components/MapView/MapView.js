ae934cf8caed3e045b3b02fe22dae075f2ef8a2e
Marce Ramirez <marceram87@gmail.com>
2018-03-29 15:25:01 +0200

    Styles for live view map

--- streetfleet/src/components/MapView/MapView.js
+++ streetfleet/src/components/MapView/MapView.js
@@ -1,22 +1,34 @@
 import React, {Component} from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 import config from '../../config';
 import MapContainer from './MapContainer';
-import VehicleModal from './../VehicleModal';
+import VehicleModal from './VehicleModal';
 import '../../css/Map.css';
 
 class MapView extends Component {
   render() {
+    const style = {
+      height: `75vh`,
+      boxShadow: `0 3px 15px rgba(0,0,0,.3)`,
+      marginTop: `2.6%`,
+    }
     return (
-      <div className="row MapView">
-        <MapContainer
-          googleMapURL={config.googleMapURL}
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `calc(100vh - 62px)` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
-        <VehicleModal/>
-      </div>
+      <Grid>
+        <Row>
+          <Col className="MapView" md={7}>
+            <MapContainer
+              googleMapURL={config.googleMapURL}
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={style} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
+          </Col>
+          <Col className="VehicleView" md={3}>
+            <VehicleModal/>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
