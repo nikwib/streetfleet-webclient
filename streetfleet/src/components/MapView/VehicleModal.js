18aa421bbf3b25908f20e34be0d71083fe69f8fc
Marce Ramirez <marceram87@gmail.com>
2018-04-02 10:58:57 +0200

    style: Added some random colors for the MapView

--- streetfleet/src/components/MapView/VehicleModal.js
+++ streetfleet/src/components/MapView/VehicleModal.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import randomColor from 'randomcolor';
 
 import '../../css/Map.css';
+import { colors } from './colors';
 
 const renderCars = (props) => {
   if (props.cars.length) {
-    return props.cars.map(car => {
+    return props.cars.map((car, i) => {
+      i++;
       return (
         <li>
           <Link to={"/CarLog/" + car._id} key={car._id} license_number={car.license_number}>
-            <div className="vColor" style={{ backgroundColor: randomColor({ luminosity: 'dark' }) }}></div>
+            <div className="vColor" style={{ backgroundColor: colors[i-1] }}></div>
             <div className="vInfo">{car.license_number.toUpperCase()}</div>
           </Link>
         </li>
