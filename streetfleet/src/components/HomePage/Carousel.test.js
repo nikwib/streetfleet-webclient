e6807b84b57a8cb8d46d6952b539651225ea83ee
Niklas Wibelius <niklas@wibelius.se>
2018-04-12 19:50:20 +0200

    Test: Add test case for Carousel

---
+++ streetfleet/src/components/HomePage/Carousel.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+import Carousel from './Carousel';
+
+describe('Carousel',() => { 
+  let comp = mount(<Carousel />);
+   it ('render the components title', () => {
+    expect(comp.find('h1').text()).toEqual('Track and manage your vehicles');
+  })
+  it ('renders the right image', () => {
+    expect(comp.find('img').exists()).toBe(true);
+  })
+})
